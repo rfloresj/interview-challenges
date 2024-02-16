ee05d14ff1d66200bc344326be247b369207b723
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-16 10:37:26 -0600

    init:Challenges

--- ejercicios-algoritmos/sesion-1/elementos-pares/index.ts
+++ ejercicios-algoritmos/sesion-1/elementos-pares/index.ts
@@ -1,4 +1,22 @@
-export default function filtrarPares(array: unknown[]): unknown[] {
+export default function filtrarPares(array: string[]): string[] {
   // TODO: implement
-  return [];
+  const appearances = array.reduce((map, letter) => {
+    if (!map[letter]) {
+      map[letter] = 0;
+    }
+
+    map[letter]++;
+
+    return map;
+  }, {});
+
+  const elements = Object.entries(appearances);
+
+  return elements.reduce((moreItems: string[], [letter, appearances]) => {
+    if (Number(appearances) % 2 === 0) {
+      moreItems.push(String(letter));
+    }
+
+    return moreItems;
+  }, []);
 }
