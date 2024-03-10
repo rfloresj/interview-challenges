ce768ea34909b4a291912abd8ac0c898637ee949
rflores <104962825+rfloresj@users.noreply.github.com>
2024-03-09 20:44:33 -0600

    elementos-pares:solved

--- ejercicios-algoritmos/sesion-1/elementos-pares/index.ts
+++ ejercicios-algoritmos/sesion-1/elementos-pares/index.ts
@@ -1,22 +1,62 @@
 export default function filtrarPares(array: string[]): string[] {
-  // TODO: implement
   const appearances = array.reduce((map, letter) => {
     if (!map[letter]) {
       map[letter] = 0;
     }
-
     map[letter]++;
-
     return map;
   }, {});
 
-  const elements = Object.entries(appearances);
-
-  return elements.reduce((moreItems: string[], [letter, appearances]) => {
-    if (Number(appearances) % 2 === 0) {
-      moreItems.push(String(letter));
-    }
-
-    return moreItems;
-  }, []);
+  return Object.keys(appearances).filter(
+    (letter) => appearances[letter] % 2 === 0
+  );
 }
+
+// My solution
+
+// export default function filtrarPares(
+//   array: Array<string | number>
+// ): Array<string | number> {
+//   // TODO: implement
+//   const duplicateItems = array.filter(
+//     (element, index) => array.indexOf(element) !== index
+//   );
+//   const onlyDuplicates = new Set(duplicateItems);
+//   return [...onlyDuplicates];
+// }
+
+// Goncy solution
+
+// export default function filtrarPares(array: string[]): string[] {
+//   const appearances = array.reduce((map, letter) => {
+//     if (!map[letter]) {
+//       map[letter] = 0;
+//     }
+//     map[letter]++;
+//     return map;
+//   }, {});
+
+//   const elements = Object.entries(appearances);
+
+//   return elements.reduce((onlyPairRepeated, [letter, appearances]) => {
+//     Number(appearances) % 2 === 0
+//       ? onlyPairRepeated.push(String(letter))
+//       : null;
+
+//     return onlyPairRepeated;
+//   }, []);
+// }
+// Goncy solution 2
+// export default function filtrarPares(array: string[]): string[] {
+//   const appearances = array.reduce((map, letter) => {
+//     if (!map[letter]) {
+//       map[letter] = 0;
+//     }
+//     map[letter]++;
+//     return map;
+//   }, {});
+
+//   return Object.keys(appearances).filter(
+//     (letter) => appearances[letter] % 2 === 0
+//   );
+// }
--- ejercicios-algoritmos/sesion-1/isograma/index.ts
+++ ejercicios-algoritmos/sesion-1/isograma/index.ts
@@ -1,4 +1,22 @@
 export default function esIsograma(string: string): boolean {
   // TODO: implement
-  return false;
+  const draft = string
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[^a-z]/g, "");
+  return new Set([...draft]).size === string.length;
 }
+
+// My solution
+// const lowerCase = string.toLocaleLowerCase();
+// const repeatWords = new Set([...lowerCase]);
+// console.log(repeatWords);
+// const splitWord = lowerCase.split("");
+// if (splitWord.includes(" ")) {
+//   return false;
+// } else if (splitWord.length == Array.from(repeatWords).length) {
+//   return true;
+// }
+// return false;
+
+// 1st solution
