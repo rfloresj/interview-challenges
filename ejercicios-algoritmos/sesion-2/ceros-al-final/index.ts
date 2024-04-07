55115294defea313297838bd8e964b99490ee2df
rflores <104962825+rfloresj@users.noreply.github.com>
2024-04-07 11:16:31 -0600

    ceros-al-final:gonzy-solution1

--- ejercicios-algoritmos/sesion-2/ceros-al-final/index.ts
+++ ejercicios-algoritmos/sesion-2/ceros-al-final/index.ts
@@ -1,13 +1,29 @@
 export default function moverCeros(array: unknown[]): unknown[] {
   // TODO: implement
-  let newArray: unknown[] = [];
-  let newArray2: unknown[] = [];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 0) {
-      newArray.push(array[i] as number);
+  const zeros: number[] = [];
+  const noZeros: unknown[] = [];
+  for (const elem of array) {
+    if (elem === 0) {
+      zeros.push(elem);
     } else {
-      newArray2.push(array[i]);
+      noZeros.push(elem);
     }
   }
-  return newArray2.concat(newArray);
+  return [...noZeros, ...zeros];
 }
+
+// My solution
+
+// export default function moverCeros(array: unknown[]): unknown[] {
+//   // TODO: implement
+//   let newArray: unknown[] = [];
+//   let newArray2: unknown[] = []
+//   for(let i = 0; i < array.length; i++){
+//     if(array[i] === 0){
+//       newArray.push(array[i])
+//     } else {
+//       newArray2.push(array[i])
+//     }
+//   }
+//   return newArray2.concat(newArray)
+// }
