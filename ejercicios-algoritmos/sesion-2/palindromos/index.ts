dbce9a700743cab01933a14f261065c9376e574a
rflores <104962825+rfloresj@users.noreply.github.com>
2024-04-22 10:23:02 -0600

    palindromos:Gonzy-solution

--- ejercicios-algoritmos/sesion-2/palindromos/index.ts
+++ ejercicios-algoritmos/sesion-2/palindromos/index.ts
@@ -1,4 +1,16 @@
 export default function posiblePalindromo(num: number): boolean {
   // TODO: implement
-  return false;
+  const result = Object.values(
+    String(num)
+      .split("")
+      .reduce(
+        (acc, num) => ({
+          ...acc,
+          [num]: acc[num] === undefined ? true : !acc[num],
+        }),
+        {}
+      )
+  );
+
+  return result.indexOf(false) === result.lastIndexOf(false);
 }
