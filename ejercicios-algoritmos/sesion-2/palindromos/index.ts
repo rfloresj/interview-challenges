dcab11195bd68380341a4c94710bf9cf201dd08d
rfloresj <104962825+rfloresj@users.noreply.github.com>
2024-04-27 19:50:48 -0600

    palindromos:Gonzy-solution:re-factor

--- ejercicios-algoritmos/sesion-2/palindromos/index.ts
+++ ejercicios-algoritmos/sesion-2/palindromos/index.ts
@@ -1,16 +1,36 @@
 export default function posiblePalindromo(num: number): boolean {
-  // TODO: implement
-  const result = Object.values(
-    String(num)
-      .split("")
-      .reduce(
-        (acc, num) => ({
-          ...acc,
-          [num]: acc[num] === undefined ? true : !acc[num],
-        }),
-        {}
-      )
-  );
+  // "1221"
+  const map = String(num)
+    // ["1", "2", "2", "1"]
+    .split("")
+    // {"1": true, "2": true}
+    .reduce(
+      (acc, num) => ({
+        ...acc,
+        [num]: acc[num] === undefined ? false : !acc[num],
+      }),
+      {}
+    );
+  // [true, true]
+  const valids = Object.values(map);
 
-  return result.indexOf(false) === result.lastIndexOf(false);
+  // -1, -1 -> true
+  return valids.indexOf(false) === valids.lastIndexOf(false);
 }
+
+// export default function posiblePalindromo(num: number): boolean {
+//   // TODO: implement
+//   const result = Object.values(
+//     String(num)
+//       .split("")
+//       .reduce(
+//         (acc, num) => ({
+//           ...acc,
+//           [num]: acc[num] === undefined ? true : !acc[num],
+//         }),
+//         {}
+//       )
+//   );
+
+//   return result.indexOf(false) === result.lastIndexOf(false);
+// }
