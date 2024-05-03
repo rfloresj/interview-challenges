cf24a844e86d63de8649bf8454c4dd9455896cb8
rfloresj <104962825+rfloresj@users.noreply.github.com>
2024-05-03 16:38:52 -0600

    contarLosLenguajes:Gonzy-solution

--- ejercicios-algoritmos/sesion-4/contar-los-lenguajes/index.ts
+++ ejercicios-algoritmos/sesion-4/contar-los-lenguajes/index.ts
@@ -2,5 +2,13 @@ import type { Developer } from "./types";
 
 export default function contarLosLenguajes(developers: Developer[]): Record<string, number> {
   // TODO: implementar
-  return {};
+  return developers.reduce((languages, {language}) => {
+    if(!languages[language]){
+      languages[language] = 0
+    }
+    
+    languages[language]++
+    // {}
+    return languages;
+  }, {})
 }
