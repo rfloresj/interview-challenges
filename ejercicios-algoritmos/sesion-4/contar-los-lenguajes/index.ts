8749a716779250f00907e88f2b3012db4c072035
rfloresj <104962825+rfloresj@users.noreply.github.com>
2024-05-05 12:58:50 -0600

    contarLosLenguajes:Gonzy-2nd-solution

--- ejercicios-algoritmos/sesion-4/contar-los-lenguajes/index.ts
+++ ejercicios-algoritmos/sesion-4/contar-los-lenguajes/index.ts
@@ -1,14 +1,29 @@
 import type { Developer } from "./types";
 
 export default function contarLosLenguajes(developers: Developer[]): Record<string, number> {
-  // TODO: implementar
-  return developers.reduce((languages, {language}) => {
-    if(!languages[language]){
-      languages[language] = 0
+  const languages = new Map();
+  for (const { language } of developers) {
+    if (!languages.has(language)) {
+      languages.set(language, 0);
     }
-    
-    languages[language]++
-    // {}
-    return languages;
-  }, {})
+    languages.set(language, languages.get(language) + 1);
+  }
+  return Object.fromEntries(languages.entries());
 }
+
+// Gonzy 1st solution
+
+// import type { Developer } from "./types";
+
+// export default function contarLosLenguajes(developers: Developer[]): Record<string, number> {
+//   // TODO: implementar
+//   return developers.reduce((languages, {language}) => {
+//     if(!languages[language]){
+//       languages[language] = 0
+//     }
+
+//     languages[language]++
+//     // {}
+//     return languages;
+//   }, {})
+// }
